a56c198ab1b511662f49d1b5725264718faa16ee
gouzal <larbi-inco@live.fr>
2019-08-09 10:55:04 +0100

    add models relashionship

--- gouzal/larapermissions/src/Permission.php
+++ gouzal/larapermissions/src/Permission.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    /**
+     * Get the role that owns the permission.
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }
--- gouzal/larapermissions/src/Role.php
+++ gouzal/larapermissions/src/Role.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * Get the permissions for the role.
+     */
+    public function permissions()
+    {
+        return $this->hasMany('App\Permission');
+    }
+
+    /**
+     * The users that belong to the role.
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
