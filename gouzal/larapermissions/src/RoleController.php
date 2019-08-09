33cc5846eaf24dc3ab47ed54cc957f5b0ed09ac3
gouzal <larbi-inco@live.fr>
2019-08-09 12:57:46 +0100

    import Controller

--- gouzal/larapermissions/src/RoleController.php
+++ gouzal/larapermissions/src/RoleController.php
@@ -4,6 +4,7 @@ namespace Gouzal\LaraPermissions;
 
 use App\Role;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RoleController extends Controller
 {
@@ -14,7 +15,7 @@ class RoleController extends Controller
      */
     public function index()
     {
-        
+        return "hello";
     }
 
     /**
